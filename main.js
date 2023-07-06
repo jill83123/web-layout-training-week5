867dece17818a2d5e341dcb26ee5abc9570556af
fang <jill83123@gmail.com>
2023-07-06 15:00:46 +0800

    Ver.1

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-console.log("Hello world!");
+//nav
+document.querySelectorAll('.nav-link').forEach((link) => {
+    if (link.href === window.location.href) {
+        link.classList.add('active');
+        link.setAttribute('aria-current', 'page');
+        return false;
+    }
+    console.log(window.location.href);
+});
+
+//ck-editor
+ClassicEditor
+    .create(document.querySelector('#editor'))
+    .catch(error => {
+        console.error(error);
+    });
